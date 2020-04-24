3fafc9adfd419653355d44719da8763754c973c6
Leonidas Yopan <leonidasyopan@gmail.com>
2020-04-24 16:41:26 -0300

    1 through 100 - problems with teens

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,19 +3,155 @@ import { Router } from "express";
 const routes = Router();
 
 routes.post("/", (request, response) => {
+  // Store the natural number sent by the user
   const naturalNumber = request.query.translate;
   console.log(naturalNumber);
 
+  // Access the translate functions responsible for giruing out
+  // how to translate the number properly
   const translated = translate(Number(naturalNumber));
   console.log(translated);
 
+  // Return the translation so the API can show it to the user
   response.json({ translation: translated });
 });
 
 function translate(naturalNumber: number) {
   let numberTranslated = "";
 
-  switch (naturalNumber) {
+  let separatedDigits = ("" + naturalNumber).split("");
+
+  let decimalPlaces = separatedDigits.length;
+
+  switch (decimalPlaces) {
+    case 1:
+      switch (naturalNumber) {
+        case 0:
+          numberTranslated = "zero";
+          break;
+        case 1:
+          numberTranslated = "one";
+          break;
+        case 2:
+          numberTranslated = "two";
+          break;
+        case 3:
+          numberTranslated = "three";
+          break;
+        case 4:
+          numberTranslated = "four";
+          break;
+        case 5:
+          numberTranslated = "five";
+          break;
+        case 6:
+          numberTranslated = "six";
+          break;
+        case 7:
+          numberTranslated = "seven";
+          break;
+        case 8:
+          numberTranslated = "eight";
+          break;
+        case 9:
+          numberTranslated = "nine";
+          break;
+        default:
+          numberTranslated = "Sorry, couldn't handle this number";
+          break;
+      }
+      break;
+    case 2:
+      let firstDigitTranslated = "";
+      const firstDigit = Number(separatedDigits[0]);
+      const lastDigit = Number(separatedDigits[1]);
+      switch (firstDigit) {
+        case 1:
+          firstDigitTranslated = "one";
+          break;
+        case 2:
+          firstDigitTranslated = "twenty";
+          break;
+        case 3:
+          firstDigitTranslated = "thirty";
+          break;
+        case 4:
+          firstDigitTranslated = "fourty";
+          break;
+        case 5:
+          firstDigitTranslated = "fifty";
+          break;
+        case 6:
+          firstDigitTranslated = "sixty";
+          break;
+        case 7:
+          firstDigitTranslated = "seventy";
+          break;
+        case 8:
+          firstDigitTranslated = "eighty";
+          break;
+        case 9:
+          firstDigitTranslated = "ninety";
+          break;
+        default:
+          firstDigitTranslated = "Sorry, couldn't handle this number";
+          break;
+      }
+      const lastDigitTranslated = oneThroughNince(lastDigit);
+      numberTranslated = firstDigitTranslated + lastDigitTranslated;
+      break;
+  }
+
+  return numberTranslated;
+}
+
+function oneThroughNince(lastDigit: number) {
+  let lastDigitTranslated = "";
+
+  switch (lastDigit) {
+    case 1:
+      lastDigitTranslated = "-one";
+      break;
+    case 2:
+      lastDigitTranslated = "-two";
+      break;
+    case 3:
+      lastDigitTranslated = "-three";
+      break;
+    case 4:
+      lastDigitTranslated = "-four";
+      break;
+    case 5:
+      lastDigitTranslated = "-five";
+      break;
+    case 6:
+      lastDigitTranslated = "-six";
+      break;
+    case 7:
+      lastDigitTranslated = "-seven";
+      break;
+    case 8:
+      lastDigitTranslated = "-eight";
+      break;
+    case 9:
+      lastDigitTranslated = "-nine";
+      break;
+    default:
+      lastDigitTranslated = "";
+      break;
+  }
+
+  return lastDigitTranslated;
+}
+
+export default routes;
+
+/* 
+
+switch (naturalNumber) {
+    case 0:
+      numberTranslated = "zero";
+      break;
     case 1:
       numberTranslated = "one";
       break;
@@ -150,7 +286,4 @@ function translate(naturalNumber: number) {
       break;
   }
 
-  return numberTranslated;
-}
-
-export default routes;
+*/
