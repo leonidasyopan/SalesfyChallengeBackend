68a8458d50fd0cee5ffa8f5692122cd883096f0a
Leonidas Yopan <leonidasyopan@gmail.com>
2020-04-29 00:41:08 -0300

    removed the switch statements

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -246,69 +246,31 @@ function hundredToString(arrayOfParts: Array<string>) {
 function oneThroughNineTeen(lastDigit: number) {
   let result = "";
 
-  switch (lastDigit) {
-    case 1:
-      result = "one";
-      break;
-    case 2:
-      result = "two";
-      break;
-    case 3:
-      result = "three";
-      break;
-    case 4:
-      result = "four";
-      break;
-    case 5:
-      result = "five";
-      break;
-    case 6:
-      result = "six";
-      break;
-    case 7:
-      result = "seven";
-      break;
-    case 8:
-      result = "eight";
-      break;
-    case 9:
-      result = "nine";
-      break;
-    case 10:
-      result = "ten";
-      break;
-    case 11:
-      result = "eleven";
-      break;
-    case 12:
-      result = "twelve";
-      break;
-    case 13:
-      result = "thirteen";
-      break;
-    case 14:
-      result = "fourteen";
-      break;
-    case 15:
-      result = "fifteen";
-      break;
-    case 16:
-      result = "sixteen";
-      break;
-    case 17:
-      result = "seventeen";
-      break;
-    case 18:
-      result = "eighteen";
-      break;
-    case 19:
-      result = "nineteen";
-      break;
-    default:
-      result = "";
-      break;
-  }
+  const number = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
 
+  for (let i = 0; i < number.length; i++) {
+    result = number[lastDigit - 1];
+  }
   return result;
 }
 
@@ -316,35 +278,22 @@ function oneThroughNineTeen(lastDigit: number) {
 // make it more readable.
 function twoDigits(preLastDigit: number) {
   let result = "";
-  switch (preLastDigit) {
-    case 2:
-      result = "twenty";
-      break;
-    case 3:
-      result = "thirty";
-      break;
-    case 4:
-      result = "fourty";
-      break;
-    case 5:
-      result = "fifty";
-      break;
-    case 6:
-      result = "sixty";
-      break;
-    case 7:
-      result = "seventy";
-      break;
-    case 8:
-      result = "eighty";
-      break;
-    case 9:
-      result = "ninety";
-      break;
-    default:
-      result = "";
-      break;
+
+  const numbers = [
+    "twenty",
+    "thirty",
+    "fourty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  for (let i = 0; i < numbers.length; i++) {
+    result = numbers[preLastDigit - 2];
   }
+
   return result;
 }
 
